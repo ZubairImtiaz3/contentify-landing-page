2900f6d0bfd0914e4fce85656f7d944bf84c58a1
ZubairImtiaz3 <zubairimtiaz395@gmail.com>
2024-03-02 03:37:07 +0500

    added gradient

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
-            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none title [background:var(--gradient)] !bg-clip-text text-transparent !bg-cover !bg-center transition-all leading-none uppercase">
               Code with confidence
             </h1>
             <p className="mx-auto max-w-[700px] text-gray-500 dark:text-gray-400">
